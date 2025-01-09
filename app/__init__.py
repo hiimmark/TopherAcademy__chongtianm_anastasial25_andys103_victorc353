a4dd1590143157dfb070d81b39fadafd0995e9b1
Mark Ma <markworldamc@gmail.com>
2025-01-09 11:00:53 -0500

    a

--- app/__init__.py
+++ app/__init__.py
@@ -6,20 +6,25 @@ P02: Devo Dining
 Time Spent: 998244353 hours
 """
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import calendar, os
 from datetime import datetime
+import db
 
 app = Flask(__name__)
 
 # HOME PAGE, SHOULD PROMPT REGISTER OR LOGIN
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template(".html")
+    return render_template("home.html")
 
 @app.route('/restaurants')
 def restaurants():
-    return render_template("restaurants.html")
+    mode = "manager" # session["mode"]
+    name = "bob" # session["name"]
+    li = db.getRestaurants()
+
+    return render_template("restaurants.html", mode = mode, name = name, li = li)
 
 def get_times(restaurant, time, numpeople):
     # return a 2d list of [[table ID, time]...]
