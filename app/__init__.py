9f2b647a734c23ef6f8c03b90497cee2d9ef6fa1
Victor Casado <vcasado50@stuy.edu>
2025-01-13 10:41:51 -0500

    ??? alee help

--- app/__init__.py
+++ app/__init__.py
@@ -12,11 +12,20 @@ from datetime import datetime
 import db
 
 app = Flask(__name__)
-
+app.secret_key = 'your_secret_key'
 # HOME PAGE, SHOULD PROMPT REGISTER OR LOGIN
+
 @app.route('/', methods=['GET', 'POST'])
-def home():
-    return render_template("home.html")
+def homeBase():
+    if('accountType' in session):
+        return render_template("home.html")
+    return redirect(url_for('logout'))
+
+@app.route('/logout')
+def logout():
+    session.pop('email', None)
+    session.pop('accountType', None)
+    return redirect(url_for('login'))
 
 @app.route('/restaurants')
 def restaurants():
@@ -24,6 +33,21 @@ def restaurants():
     name = "bob" # session["name"]
     li = db.getRestaurants()
     return render_template("restaurants.html", mode = mode, name = name, li = li)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    print("AAA")
+    if request.method == 'POST':
+        print("BBB")
+        email = request.form['email']
+        password = request.form['password']
+        if db.checkLogin(email, password) == False:
+            return render_template("login.html")
+        session['email'] = email
+        session['accountType'] = db.checkLogin(email, password)
+        return redirect(url_for(''))
+    return render_template("login.html")
+
 
 def get_times(restaurant, time, numpeople):
     # return a 2d list of [[table ID, time]...]
